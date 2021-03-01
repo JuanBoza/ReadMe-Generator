5c4a727b722a9821070479c85e2e8f44504dce67
Juan Boza <Juanboza80@gmail.com>
2021-03-01 14:32:29 -0700

    default removed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer"); 
 const fs = require("fs"); 
-const generateMarkdown = require("./utils/generateMarkdown.js").default
+const generateMarkdown = require("./utils/generateMarkdown.js"); 
 
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,40 @@
 function generateMarkdown(data) {
-    return `# ${data.title}
-  
-  `;
-  }
-  
-  module.exports = generateMarkdown;
-  
+  return `# ${data.title}
+
+
+
+Title of project
+ ${data.projectTitle}
+
+Description 
+ ${data.description}
+
+Installation Process 
+ ${data.installation}
+
+Projects intended use 
+ ${data.usage}
+
+Licenses Used 
+ ${data.license}
+
+Names of contributors
+ ${data.contributors}
+
+Tests 
+ ${data.tests}
+
+Any Questions
+ ${data.questions}
+
+Github username
+ ${data.username}
+
+Contact information
+${data.contactInfo}
+
+  `; 
+
+}
+
+export default generateMarkdown; 
